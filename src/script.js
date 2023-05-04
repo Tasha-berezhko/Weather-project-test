c3c6fbf6199aadd53798b928e135485fbab66169
Tasha-berezhko <124585701+Tasha-berezhko@users.noreply.github.com>
2023-05-04 10:56:26 +0300

    updated apikey

--- src/script.js
+++ src/script.js
@@ -105,7 +105,7 @@ search("Shanghai");
 //geolocation
 
 function searchLocation(position) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "e8dec81490f528b6b7847357fa83bb2a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayWeather);
